97eb6f94269d83d937bc8a4ed2e7db089eb9cf7e
Tynan <quixotichamster@gmail.com>
2017-02-24 17:27:56 -0600

    Spring Initialzr

---
+++ src/main/java/com/fabhelper/FabhelperApplication.java
@@ -0,0 +1,12 @@
+package com.fabhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabhelperApplication.class, args);
+	}
+}
